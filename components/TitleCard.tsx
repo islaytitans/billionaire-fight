183b141257830467b49417fb063a350375f805f0
Jonathan Robbins <Jonathan.Robbins@rcn.org.uk>
2021-12-08 09:17:40 +0000

    Added responsive design to TitleCard

--- components/TitleCard.tsx
+++ components/TitleCard.tsx
@@ -10,8 +10,8 @@ const TitleCard = ({
 }) => {
   return (
     <section className="h-48 flex flex-col justify-around items-center">
-      <figure className="pb-20 md:pb-80">
-        <Image src="/images/Logo.png" alt={title} width={300} height={40} />
+      <figure className="px-2 pb-20 md:pb-80">
+        <Image src="/images/Logo.png" alt={title} width={600} height={80} />
       </figure>
       <Link href="/fighters">
         <a className="text-2xl md:text-4xl">{enterText}</a>
