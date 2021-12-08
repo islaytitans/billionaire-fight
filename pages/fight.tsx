b8a93e492c9d217a174a3c62dfc6b5bbe8acee6b
Jonathan Robbins <Jonathan.Robbins@rcn.org.uk>
2021-12-08 15:43:01 +0000

    Fleshed out the fight page

--- pages/fight.tsx
+++ pages/fight.tsx
@@ -1,8 +1,28 @@
 import { NextPage } from "next";
+import Head from "next/head";
+import React from "react";
+import Header from "../components/Header";
 import PageTitle from "../components/PageTitle";
+import SelectedFighters from "../components/SelectedFighters";
 
 const Fight: NextPage = () => {
-  return <PageTitle title="Fight!" />;
+  return (
+    <div>
+      <Head>
+        <title>Billionaire Bum Fight</title>
+        <meta name="description" content="Let there be blood" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Header />
+        <div className="flex flex-col min-h-screen items-center">
+          <PageTitle title="Let there be blood!" />
+          <SelectedFighters />
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default Fight;
