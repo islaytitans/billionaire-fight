b590fcff17b8d1ea3f2c4428ff1e9ff39e5ee339
Jonathan Robbins <Jonathan.Robbins@rcn.org.uk>
2021-12-08 12:23:05 +0000

    Added some styling to the selected fighters

--- components/SelectedFighters.tsx
+++ components/SelectedFighters.tsx
@@ -13,10 +13,14 @@ const SelectedFighters = () => {
   };
 
   return (
-    <section>
-      <h1>Choose your fighters</h1>
-      {Players.player1Id}
-      {Players.player2Id}
+    <section className="flex flex-col py-4 px-2 items-center text-center bg-gray-50 rounded-xl shadow-lg">
+      <h1 className="text-4xl">Select your fighters!</h1>
+      <div className="flex flex-col my-2 px-1 bg-gray-500">
+        <h2 className="text-2xl">{Players.player1Id}</h2>
+        <span className="text-3xl my-3">VS</span>
+        <h2 className="text-2xl">{Players.player2Id}</h2>
+      </div>
+
       <button onClick={handleOnClick1}>Select player 1</button>
       <button onClick={handleOnClick2}>Select player 2</button>
     </section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
-  mode: 'jit',
+  mode: "jit",
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        lg: "0 10px 15px -3px rgba(220, 38, 38, 0.5), 0 4px 6px -2px rgba(220, 38, 38, 0.1)",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
