70a5818068d65e808dc6a52d7ab1585cfa92cec2
Jonathan Robbins <Jonathan.Robbins@rcn.org.uk>
2021-12-08 15:57:26 +0000

    Added simple code to disable button

--- components/StartFight.tsx
+++ components/StartFight.tsx
@@ -1,8 +1,18 @@
 import Link from "next/link";
+import { useContext } from "react";
+import { PlayersContext } from "../context/PlayersContext";
 
 const StartFight = () => {
+  const Players = useContext(PlayersContext);
+
+  const disableButton =
+    Players.player1Id === null || Players.player2Id === null;
+
   return (
-    <button className="w-96 py-3 my-2 text-4xl tracking-widest bg-red-600 hover:bg-red-700 text-yellow-300 border-gray-100 rounded-xl">
+    <button
+      disabled={disableButton}
+      className="w-96 py-3 my-2 text-4xl tracking-widest bg-red-600 hover:bg-red-700 text-yellow-300 border-gray-100 rounded-xl disabled:opacity-50"
+    >
       <Link href="/fight">
         <a>Fight!</a>
       </Link>
