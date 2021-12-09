420b14c2577a7405566a916766969b3995e2f080
Jonathan Robbins <Jonathan.Robbins@rcn.org.uk>
2021-12-09 16:27:08 +0000

    basic implementation of a round

--- components/match/Ticker.tsx
+++ components/match/Ticker.tsx
@@ -1,21 +1,63 @@
-import { useContext } from "react";
+import { useContext, MouseEvent } from "react";
 import { GameContext } from "../../context/GameContext";
-import Fighter from "../../types/Fighter";
+import Fighter, { getModifier } from "../../types/Fighter";
 
 const Ticker = ({
   fighter1,
   fighter2,
 }: {
-  fighter1: Fighter;
-  fighter2: Fighter;
+  fighter1: Fighter | null;
+  fighter2: Fighter | null;
 }) => {
   const Game = useContext(GameContext);
+
+  if (fighter1 === null || fighter2 === null) {
+    return <h1>No fighters selected</h1>;
+  }
+
+  const fighter1SpeedModifier = getModifier(fighter1?.speed);
+  const fighter2SpeedModifier = getModifier(fighter2?.speed);
+
+  const determineInitiative = () => {
+    const fighter1Speed = Math.floor(Math.random() * 20 + 1);
+    const fighter2Speed = Math.floor(Math.random() * 20 + 1);
+
+    if (
+      fighter1Speed + fighter1SpeedModifier >
+      fighter2Speed + fighter2SpeedModifier
+    ) {
+      return { attacker: fighter1, defender: fighter2 };
+    } else if (
+      fighter1Speed + fighter1SpeedModifier <
+      fighter2Speed + fighter2SpeedModifier
+    ) {
+      return { attacker: fighter2, defender: fighter1 };
+    } else {
+      const flip = Math.floor(Math.random() * 2 + 1);
+      if (flip === 1) {
+        return { attacker: fighter1, defender: fighter2 };
+      } else {
+        return { attacker: fighter2, defender: fighter1 };
+      }
+    }
+  };
+
+  const handleOnClick = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    const { attacker, defender } = determineInitiative();
+
+    fighter2.currentWealth = fighter2.currentWealth - 50;
+    console.log(fighter2);
+  };
 
   return (
     <>
       <h1>Ticker</h1>
       <h2>{fighter1?.nickname}</h2>
+      <p>{fighter1?.currentWealth}</p>
       <h2>{fighter2?.nickname}</h2>
+      <p>{fighter2?.currentWealth}</p>
+      <button onClick={handleOnClick}>Next Round</button>
     </>
   );
 };
--- types/Fighter.ts
+++ types/Fighter.ts
@@ -1,12 +1,18 @@
-interface Fighter {
+type Fighter = {
   id: number;
   name: string;
   nickname: string;
   wealth: number;
+  currentWealth: number;
   strength: number;
   defence: number;
   speed: number;
   image: string;
+};
+
+export function getModifier(score: number): number {
+  const mod = Math.floor(score / 2 - 5);
+  return mod;
 }
 
 export default Fighter;
