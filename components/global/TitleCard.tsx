d41293709ace1c26b788c9096c30f288311a0c7b
jonathan robbins <jonathan.m.robbins@gmail.com>
2022-01-02 15:45:40 +0000

    Improved spacing on titleCard

--- components/global/TitleCard.tsx
+++ components/global/TitleCard.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 
 const TitleCard = ({ title, enterText }: { title: string; enterText: string }) => {
   return (
-    <section className="flex flex-col min-h-screen items-center p-6 bg-red-600 text-yellow-300">
-      <figure className="">
+    <section className="flex flex-col min-h-screen justify-around items-center p-6 bg-red-600 text-yellow-300">
+      <figure>
         <Image src="/images/Logo.png" alt={title} width={600} height={80} />
       </figure>
       <Link href="/fighters">
